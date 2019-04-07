800b37dc15d99d7af6c1e86168a0a87e29650c9e
Bea Bernadette Concepcion <beabae1111@gmail.com>
2019-04-07 13:34:43 +0800

    Create header.php

---
+++ app/views/header.php
@@ -0,0 +1,108 @@
+
+
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+
+  <link href="./assets/css/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+ <!-- CSS Files -->
+  <link href="./assets/css/material-dashboard.css?v=2.1.0" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="./assets/demo/demo.css" rel="stylesheet" />
+
+<!-- data tables css-->
+<link rel="stylesheet" type="text/css" href="./assets/DataTables/datatables.min.css"/>
+
+<!--leaflet js resource -->
+   <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7/leaflet.css"/>
+
+        <script 
+          src="http://cdn.leafletjs.com/leaflet-0.7/leaflet.js"> 
+        </script>
+
+
+<!--   Pulsing icon   -->
+<link rel="stylesheet" href="https://cdn.rawgit.com/mapshakers/leaflet-icon-pulse/add42abc/src/L.Icon.Pulse.css">
+<script src='https://cdn.rawgit.com/mapshakers/leaflet-icon-pulse/master/src/L.Icon.Pulse.js'></script>
+
+<!--   Core JS Files   -->
+<script src="./assets/js/core/jquery.min.js" type="text/javascript"></script>
+<script src="./assets/js/core/popper.min.js" type="text/javascript"></script>
+<script src="./assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+
+<!-- Plugin for the Perfect Scrollbar -->
+<script src="./assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+
+<!-- Plugin for the momentJs  -->
+<script src="./assets/js/plugins/moment.min.js"></script>
+
+<!--  Plugin for Sweet Alert -->
+<script src="./assets/js/plugins/sweetalert2.js"></script>
+
+<!-- Forms Validations Plugin -->
+<script src="./assets/js/plugins/jquery.validate.min.js"></script>
+
+<!--  Plugin for the Wizard, full documentation here: https://github.com/VinceG/twitter-bootstrap-wizard -->
+<script src="./assets/js/plugins/jquery.bootstrap-wizard.js"></script>
+
+<!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
+<script src="./assets/js/plugins/bootstrap-selectpicker.js" ></script>
+
+<!--  Plugin for the DateTimePicker, full documentation here: https://eonasdan.github.io/bootstrap-datetimepicker/ -->
+<script src="./assets/js/plugins/bootstrap-datetimepicker.min.js"></script>
+
+<!--  DataTables.net Plugin, full documentation here: https://datatables.net/    -->
+<script src="./assets/js/plugins/jquery.datatables.min.js"></script>
+
+<!--	Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
+<script src="./assets/js/plugins/bootstrap-tagsinput.js"></script>
+
+<!-- Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
+<script src="./assets/js/plugins/jasny-bootstrap.min.js"></script>
+
+<!--  Full Calendar Plugin, full documentation here: https://github.com/fullcalendar/fullcalendar    -->
+<script src="./assets/js/plugins/fullcalendar.min.js"></script>
+
+<!-- Vector Map plugin, full documentation here: http://jvectormap.com/documentation/ -->
+<script src="./assets/js/plugins/jquery-jvectormap.js"></script>
+
+<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+<script src="./assets/js/plugins/nouislider.min.js" ></script>
+
+<!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support SweetAlert -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
+
+<!-- Library for adding dinamically elements -->
+<script src="./assets/js/plugins/arrive.min.js"></script>
+
+<!-- Chartist JS -->
+<script src="./assets/js/plugins/chartist.min.js"></script>
+
+<!--  Notifications Plugin    -->
+<script src="./assets/js/plugins/bootstrap-notify.js"></script>
+
+
+  <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="./assets/js/material-dashboard.min.js?v=2.1.0" type="text/javascript"></script>
+<script src="./assets/demo/demo.js"></script>
+
+
+
+
+
+<!-- login css -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="./assets/css/styles.min.css">
+
+<!-- marker css -->
+<link rel="stylesheet" href="./assets/marker.css">
+
+
+<!-- marker css -->
+<link rel="stylesheet" href="./assets/date.css">
+
+<script type="text/javascript" src=".assets/datepicket/src/js/tempusdominus-bootstrap-4.min.js"></script>
+<link rel="stylesheet" href="./assets/datepicket/src/css/tempusdominus-bootstrap-4.min.css" />
+
+<script type="text/javascript" src="./app/js/websocket.js"></script>
+
