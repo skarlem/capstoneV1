9980a1479512a071c28eae100af037336bd14ba0
skarlem <lynette9543@gmail.com>
2019-05-08 22:10:02 +0800

    UI 2

--- app/views/support/map_support_nav.php
+++ app/views/support/map_support_nav.php
@@ -246,18 +246,7 @@ report_problem
           
           <div class="collapse navbar-collapse justify-content-end">
             <ul class="navbar-nav">
-          <!-- dropdown for notification--> 
-          <li class="nav-item dropdown">
-                <a class="nav-link" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <i class="material-icons">notifications</i>
-                  <p class="d-lg-none d-md-block">
-                    Some Actions
-                  </p>
-                </a>
-             
-              </li>
-             <!-- /dropdown for noticification--> 
-
+         
 
              <!-- dropdown for settings--> 
              <li class="nav-item dropdown">
